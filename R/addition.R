dfbb54ca3b7ab09aac5444e57238d8510b2cbe3b
Patrick Fournier <p_fournier@hushmail.com>
2019-09-24 22:26:17 -0400

    Initial commit.

---
+++ R/addition.R
@@ -0,0 +1,20 @@
+#' Additionne 2
+#'
+#' Ajoute 2 à un vecteur.
+#'
+#' La fonction \code{add2} ajoute 2 en son argument. Pour ce faire,
+#' elle exécute une opération mathématique connue sous le nom d'addition.
+#'
+#' L'addition est commutative. En conséquence, ses arguments peuvent commuter
+#' sans pour autant changer le résultat.
+#'
+#' @param x vecteur de nombres.
+#'
+#' @return Un vecteur contenant les même nombres que \code{x} auxquels on a
+#' ajouté 1.
+#'
+#' @examples
+#' add2(1)
+#'
+#' @export
+add2 <- function(x) x + 1
---
+++ R/exemple.R
@@ -0,0 +1,8 @@
+#' Exemple
+#'
+#' Ceci est la description du package Exemple.
+#'
+#' Les paragraphes suivants donne différents détails.
+#'
+#' Il peut y en avoir autant que l'on veut!
+"_PACKAGE"
